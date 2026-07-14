9ac3a5568b71f1cb0c872e66768410bc80f571f6
Shriramcool <shriramughade04@gmail.com>
2026-07-14 22:41:28 +0530

    Prepare for deployment

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -3,8 +3,12 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_URL) {
+	throw new Error('DATABASE_URL is not set');
+}
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(env.DATABASE_URL, {
+	ssl: 'require'
+});
 
 export const db = drizzle(client, { schema });
