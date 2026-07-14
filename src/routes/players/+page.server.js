23cc662964a52242bb13129753f15391a72f25fe
Shriramcool <shriramughade04@gmail.com>
2026-07-14 22:35:00 +0530

    Prepare for deployment

--- src/routes/players/+page.server.js
+++ src/routes/players/+page.server.js
@@ -1,56 +1,15 @@
 import { db } from '$lib/server/db';
 import { players } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm';
-import { tournamentPlayers } from '$lib/server/db/schema';
+
 export async function load() {
-	const allPlayers = await db.select().from(players);
+	try {
+		const allPlayers = await db.select().from(players);
 
-	return {
-		players: allPlayers
-	};
+		return {
+			players: allPlayers
+		};
+	} catch (err) {
+		console.error('DATABASE ERROR:', err);
+		throw err;
+	}
 }
-
-export const actions = {
-	create: async ({ request }) => {
-		const formData = await request.formData();
-
-		await db.insert(players).values({
-			name: formData.get('name'),
-			email: formData.get('email'),
-			rating: Number(formData.get('rating'))
-		});
-	},
-
-	update: async ({ request }) => {
-		const formData = await request.formData();
-
-		const id = Number(formData.get('id'));
-
-		await db
-			.update(players)
-			.set({
-				name: formData.get('name'),
-				email: formData.get('email'),
-				rating: Number(formData.get('rating'))
-			})
-			.where(eq(players.id, id));
-	},
-
-delete: async ({ request }) => {
-
-	const formData = await request.formData();
-
-	const id = Number(formData.get('id'));
-
-
-	await db
-		.delete(tournamentPlayers)
-		.where(eq(tournamentPlayers.playerId, id));
-
-
-	await db
-		.delete(players)
-		.where(eq(players.id, id));
-
-}
-};
